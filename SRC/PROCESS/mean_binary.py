3ff515967d582b54c2c9b50a1c925935ffc619e2
MansourSol <132652253+MansourSol@users.noreply.github.com>
2024-01-10 13:32:29 +0100

    Update mean_binary.py

--- SRC/PROCESS/mean_binary.py
+++ SRC/PROCESS/mean_binary.py
@@ -1,9 +1,14 @@
 import os
 def calculate_mean(values):
     return sum(values) / len(values)
+    
+#alpha_folder = "../../RESULTS/Y_pred_binary"
+#reverse_folder = "../../RESULTS/MAE after training/Reverse_TestBinaireYPRED"
 
 alpha_folder = "Y_pred_binary"
 reverse_folder = "Reverse_TestBinaireYPRED"
+
+#mean_folder = "../../RESULTS/MAE after training/MeanBinary"
 
 mean_folder = "MeanBinary"
 for filename_alpha in os.listdir(alpha_folder):
