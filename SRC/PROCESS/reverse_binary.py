41c9e5e36f3306f83fe9137db6c8ecb2d0244105
MansourSol <132652253+MansourSol@users.noreply.github.com>
2024-01-10 14:59:15 +0100

    Update reverse_binary.py

--- SRC/PROCESS/reverse_binary.py
+++ SRC/PROCESS/reverse_binary.py
@@ -12,8 +12,8 @@ def traiter_fichier(chemin_fichier):
         print(f"Résultats pour le fichier {chemin_fichier}:")
         for resultat in resultats_padded:
             print(resultat)
-            #dossier_destination = "../../RESULTS/MAE before training/Reverse_TestBinaire"
-        dossier_destination = "/Users/admin-21760/Desktop/M2/Computational Systems and Structural Biology/Bioinformatics of RNA and non-coding world/RNA_angles/RNA-project-angles/data/Reverse_TestBinaire"
+        dossier_destination = "../../RESULTS/MAE before training/Reverse_TestBinaire"
+        #dossier_destination = "/Users/admin-21760/Desktop/M2/Computational Systems and Structural Biology/Bioinformatics of RNA and non-coding world/RNA_angles/RNA-project-angles/data/Reverse_TestBinaire"
         if not os.path.exists(dossier_destination):
             os.makedirs(dossier_destination)
         nom_fichier = os.path.basename(chemin_fichier)
@@ -31,8 +31,8 @@ def traiter_fichier(chemin_fichier):
         print(f"Une erreur s'est produite : {e}")
 
 if __name__ == "__main__":
-    #dossier_a_traiter = "../../DATA MODIFIED/TrueAngles_TestBINAIRE"
-    dossier_a_traiter = "/Users/admin-21760/Desktop/M2/Computational Systems and Structural Biology/Bioinformatics of RNA and non-coding world/RNA_angles/RNA-project-angles/data/TrueAngles_TestBINAIRE"
+    dossier_a_traiter = "../../DATA MODIFIED/TrueAngles_TestBINAIRE"
+    #dossier_a_traiter = "/Users/admin-21760/Desktop/M2/Computational Systems and Structural Biology/Bioinformatics of RNA and non-coding world/RNA_angles/RNA-project-angles/data/TrueAngles_TestBINAIRE"
     fichiers_du_dossier = os.listdir(dossier_a_traiter)
     fichiers_texte = [fichier for fichier in fichiers_du_dossier if fichier.endswith(".txt")]
     for fichier in fichiers_texte:
