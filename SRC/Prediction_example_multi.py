79edcfe2fb6a3ab1aa1412ada6e0d138934eefb9
MansourSol <132652253+MansourSol@users.noreply.github.com>
2024-01-10 15:03:32 +0100

    Update Prediction_example_multi.py

--- SRC/Prediction_example_multi.py
+++ SRC/Prediction_example_multi.py
@@ -1,6 +1,6 @@
-#example_one_hot_matrix_path = '../../DATA/sample/OneHotMatrices_OutputExample/example_one_hot_matrix.npy'
+example_one_hot_matrix_path = '../../DATA/sample/OneHotMatrices_OutputExample/example_one_hot_matrix.npy'
 
-example_one_hot_matrix_path = '/Users/admin-21760/Desktop/M2/Computational Systems and Structural Biology/Bioinformatics of RNA and non-coding world/RNA_angles/RNA-project-angles/data/sample/OneHotMatrices_OutputExample/example_one_hot_matrix.npy'
+#example_one_hot_matrix_path = '/Users/admin-21760/Desktop/M2/Computational Systems and Structural Biology/Bioinformatics of RNA and non-coding world/RNA_angles/RNA-project-angles/data/sample/OneHotMatrices_OutputExample/example_one_hot_matrix.npy'
 example_one_hot_matrix = np.load(example_one_hot_matrix_path)
 example_normalized = scaler.transform(example_one_hot_matrix)
 example_prediction = mlp_multi.predict(example_normalized)
