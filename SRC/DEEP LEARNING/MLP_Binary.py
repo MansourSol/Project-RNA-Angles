04bb55c67899d768de853baa958cb8b98bdd7194
MansourSol <132652253+MansourSol@users.noreply.github.com>
2024-01-10 13:10:00 +0100

    Update MLP_Binary.py

--- SRC/DEEP LEARNING/MLP_Binary.py
+++ SRC/DEEP LEARNING/MLP_Binary.py
@@ -6,7 +6,11 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler
-#train_data_folder = "../DATA MODIFIED/OneHotMatrices_OutputTrain/"
+#train_data_folder = "../../DATA MODIFIED/OneHotMatrices_OutputTrain/"
+#test_data_folder = "../../DATA MODIFIED/OneHotMatrices_OutputTest/"
+#train_labels_folder = "../../DATA MODIFIED/TrueAngles_TrainBINAIRE/"
+#test_labels_folder = "../../DATA MODIFIED/TrueAngles_TestBINAIRE/"
+
 train_data_folder = "/Users/admin-21760/Desktop/M2/Computational Systems and Structural Biology/Bioinformatics of RNA and non-coding world/RNA_angles/RNA-project-angles/data/OneHotMatrices_OutputTrain/"
 test_data_folder = "/Users/admin-21760/Desktop/M2/Computational Systems and Structural Biology/Bioinformatics of RNA and non-coding world/RNA_angles/RNA-project-angles/data/OneHotMatrices_OutputTest/"
 train_labels_folder = "/Users/admin-21760/Desktop/M2/Computational Systems and Structural Biology/Bioinformatics of RNA and non-coding world/RNA_angles/RNA-project-angles/data/TrueAngles_TrainBINAIRE/"
